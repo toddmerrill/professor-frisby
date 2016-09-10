70b461c5e5afbbab0670d233d0856d01608231ea
Todd Merrill <github@toddmerrill.com>
2016-09-10 12:33:20 +0200

    Exercises 1 and 1a are complete

--- tests/exercise1a-test.js
+++ tests/exercise1a-test.js
@@ -4,10 +4,8 @@ var sentences =  require('../src/exercise1a');
 
 describe('partial application impl', function () {
     it('parse the words of a sentence - apply the argument before', function () {
-/*
         assert.deepEqual(sentences(['the rain in spain','falls mostly on the plain']),
                                     [['the','rain','in','spain'],['falls','mostly','on','the','plain']]);
 
-*/
     });
 });
