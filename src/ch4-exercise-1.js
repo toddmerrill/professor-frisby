23b210a8ed773dd058eef30bee6193f5940c2624
Todd Merrill <github@toddmerrill.com>
2016-09-12 16:54:32 +0200

    exercise 1 solution

--- src/ch4-exercise-1.js
+++ src/ch4-exercise-1.js
@@ -2,10 +2,6 @@ var _ = require('ramda');
 // Exercise 1
 //==============
 // Refactor to remove all arguments by partially applying the function.
-var words = function(str) {
-    return function() {
-        return _.split(' ', str);
-    };
-};
+var words = _.split(' ');
 
 module.exports = words;
--- tests/ch4-exercise-1-test.js
+++ tests/ch4-exercise-1-test.js
@@ -4,7 +4,6 @@ var words =  require('../src/ch4-exercise-1.js');
 
 describe('partial application impl', function () {
     it('parse the words of a sentence - apply the argument before', function () {
-        assert.deepEqual(words('the rain in spain')(), ['the','rain','in','spain']);
-
+        assert.deepEqual(words('the rain in spain'), ['the','rain','in','spain']);
     });
 });
