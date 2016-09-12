227b8c714f3b5c5eff33ac886b52a16e83797b32
Todd Merrill <github@toddmerrill.com>
2016-09-12 10:31:48 +0200

    Renamed files to include chapter

--- tests/ch4.exercise1-test.js
+++ tests/ch4.exercise1-test.js
@@ -1,9 +1,10 @@
 var assert = require('chai').assert;
 
-var filterQs =  require('../src/exercise2');
+var words =  require('../src/ch4.exercise1');
 
 describe('partial application impl', function () {
     it('parse the words of a sentence - apply the argument before', function () {
-        assert.deepEqual(filterQs(['banana','frog','quizshow','quiet','pizza','quack']), ['quizshow','quiet','quack']);
+        assert.deepEqual(words('the rain in spain')(), ['the','rain','in','spain']);
+
     });
 });
---
+++ tests/ch4.exercise2-test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+var filterQs =  require('../src/ch4.exercise2');
+
+describe('refactor to remove arguments', function () {
+    it('return the words from the array that contain a "q"', function () {
+        assert.deepEqual(filterQs(['banana','frog','quizshow','quiet','pizza','quack']), ['quizshow','quiet','quack']);
+    });
+});
