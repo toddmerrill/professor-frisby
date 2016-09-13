7840c3475ec922ff1d502f91eaa67ae6455213e1
Todd Merrill <github@toddmerrill.com>
2016-09-13 13:34:12 +0200

    Chapter 5 exercise 1 solution

--- src/ch05-exercise-01.js
+++ src/ch05-exercise-01.js
@@ -3,9 +3,14 @@ var _ = require('ramda');
 // Exercise 1:
 // ============
 // Use _.compose() to rewrite the function below. Hint: _.prop() is curried.
-var isLastInStock = function(cars) {
-    var last_car = _.last(cars);
-    return _.prop('in_stock', last_car);
-};
+
+// original:
+// var isLastInStock = function(cars) {
+//     var last_car = _.last(cars);
+//     return _.prop('in_stock', last_car);
+// };
+
+// solution:
+var isLastInStock = _.compose(_.prop('in_stock'), _.last);
 
 module.exports = isLastInStock;
