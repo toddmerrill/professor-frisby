c1a39a793c9611df834716137d4957c1ff17702c
Todd Merrill <github@toddmerrill.com>
2016-09-29 15:09:44 +0200

    Chapter 9 exercise 4 - added test comment

--- tests/ch09-exercise-04-test.js
+++ tests/ch09-exercise-04-test.js
@@ -4,7 +4,7 @@ var sendMail = require('../src/ch09-exercise-04');
 
 describe("Ch 09 exercise 4: Use validateEmail, addToMailingList, and emailBlast to implement " +
     "ex4's type signature", function () {
-    it('', function(){
+    it('Returns an Either with (Left) an invalid string, or (Right) an IO functor returning correct message', function(){
         var getIt = either(_.identity, unsafePerformIO);
         assert.equal(getIt(sendMail('bla@bla.com')), 'emailed: bla@bla.com');
         assert.equal(getIt(sendMail('bla')), 'invalid email');
